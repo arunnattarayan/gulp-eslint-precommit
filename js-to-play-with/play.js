7eb50dea0e9ec5077d4e3a11fc8936ab2482c23e
Or Kazaz <or.kazaz@autodesk.com>
2015-12-22 15:12:05 +0200

    Edited the play.js file

--- js-to-play-with/play.js
+++ js-to-play-with/play.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 // TODO: nothing
 var heLeo = 'world';
 
@@ -7,4 +8,5 @@ function add (param_name1, param_name_2){
 }
 
             add(1,2); // adding number 1 and number 2 = 3
+
 /* eslint-enable */
